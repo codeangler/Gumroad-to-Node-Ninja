9684a79cd5ce0650e8ff5cd8cbb87370b3ef5671
codeangler <dev@codeangler.com>
2016-11-04 15:21:29 -0600

    module export demonstration video 3.  exports.add  or module.exports= {}

--- caseys-scratchpad/app.js
+++ caseys-scratchpad/app.js
@@ -1,28 +1,14 @@
+var math = require('./math');
+
 // console.log('hello world');;
+
 // let x = process.argv[0];
 // let y = process.argv[1];
 let command = process.argv[2];
 let x = Number(process.argv[3]);
 let y = Number(process.argv[4]);
-// console.log(z, aa, ab);
 
-// let a = process.env.A;
-// let b = process.env.B;
-// let c = process.env.C;
+let value = math[command](x, y);
 
-// switch(command){
-//   case "add": 
-//     console.log( x + y ) ;
-//   case "sub": 
-//     console.log( x - y );
-//   default: console.log('please select opersation')
-// }
+console.log(value);
 
-if (command === "add") {
-  console.log( x + y);
-}
-if (command === "sub") {
-  console.log( x - y);
-}
-
-// console.log(command);
---
+++ caseys-scratchpad/math.js
@@ -0,0 +1,12 @@
+
+exports.add = function(a, b) {
+  return a + b;
+}
+
+exports.substract = function (x, y) {
+  return x - y;
+}
+
+exports.multiply = function( q, r) {
+  return q * r;
+}
